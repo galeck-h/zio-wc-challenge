2b7a62cd6dca6a67d7f928399f319ff9b82f95f8
“gal <gal.eckstein@hunters.ai>
2022-10-24 14:29:50 +0300

    v1.1

--- src/main/scala/my/zio/challenge/WindowCalc.scala
+++ src/main/scala/my/zio/challenge/WindowCalc.scala
@@ -15,23 +15,36 @@ object WindowCalc {
 
 
   def updateWindowWithEvent(ref: Ref[Seq[Data]], newData: Data): Task[Seq[Data]] = for {
-    newState <- ref.modify(state => (calcWindowState(state),calcWindowState(state)))
+    newState <- ref.modify(state => (calcWindowState(state), calcWindowState(state)))
     _ <- Console.printLine("here")
   } yield newState
 
 
   def calcWindowState(windowSeq: Seq[Data]): Seq[Data] = {
-    val currentTimestamp = currentTimeMillis()/ 1000
+    val currentTimestamp = currentTimeMillis() / 1000
+
     def predicate(ts: Long) = currentTimestamp - ts > windowLength.toSeconds
+
     windowSeq.filterNot(d => predicate(d.timestamp))
   }
 }
 
-trait CalculateFrequencyWithinWindow{
-  def ServiceCalc(windowSeq: Seq[Data]): MapView[String, MapView[String, Int]]
-}
 
-class CalculateFrequencyWithinWindowImpl extends CalculateFrequencyWithinWindow{
-  override def ServiceCalc(windowSeq: Seq[Data]): MapView[String, MapView[String, Int]] =
-    windowSeq.groupBy(_.eventType).view.mapValues(_.groupBy(_.data).view.mapValues(_.size))
+object FreqCalc {
+  trait Service {
+    def Calculator(windowSeq: Seq[Data]): Task[MapView[String, MapView[String, Int]]]
+  }
+
+//  class CalculateFrequencyWithinWindowImpl extends FreqCalc {
+//    override def Service(windowSeq: Seq[Data]): Task[MapView[String, MapView[String, Int]]] =
+//      ZIO.succeed(windowSeq.groupBy(_.eventType).view.mapValues(_.groupBy(_.data).view.mapValues(_.size)))
+//  }
+
+  val live: ZLayer[Any, Nothing, Has[FreqCalc.Service]] = ZLayer.succeed(
+    // that same service we wrote above
+    new Service {
+      override def Calculator(windowSeq: Seq[Data]): Task[MapView[String, MapView[String, Int]]] =
+        ZIO.succeed(windowSeq.groupBy(_.eventType).view.mapValues(_.groupBy(_.data).view.mapValues(_.size)))
+    }
+  )
 }
